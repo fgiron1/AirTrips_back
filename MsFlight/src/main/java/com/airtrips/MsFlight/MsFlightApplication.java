70a9209b53f8705f5e2021ffacc3275febe879d8
Victor.Irimies <victoririmies99@gmail.com>
2022-10-31 15:22:07 +0100

    First commit

---
+++ MsFlight/src/main/java/com/airtrips/MsFlight/MsFlightApplication.java
@@ -0,0 +1,13 @@
+package com.airtrips.MsFlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFlightApplication.class, args);
+	}
+
+}
---
+++ MsFlight/src/test/java/com/airtrips/MsFlight/MsFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtrips.MsFlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
