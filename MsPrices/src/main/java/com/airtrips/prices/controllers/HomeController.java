a862bf53d636d9b87f1b93149cba8a7d60be1275
INT\Fernando.Giron <fernando.giron@solera.com>
2022-11-02 19:41:11 +0100

    Completed /prices endpoint interface, added conversion of parameters to corresponding types + validation logic

--- MsPrices/src/main/java/com/airtrips/prices/controllers/HomeController.java
+++ MsPrices/src/main/java/com/airtrips/prices/controllers/HomeController.java
@@ -9,17 +9,44 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.time.ZonedDateTime;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/api/v1/price")
-    public ResponseEntity<String> calculatePrice(@RequestParam(required = false, name = "hasLuggage") String hasLuggage,
-                                                 @RequestParam(required = false, name = "departureDate") String departureDate,
-                                                 @RequestParam(required = false, name = "arrivalDate") String arrivalDate
+    public ResponseEntity<String> calculatePrice(@RequestParam(name = "hasLuggage") String hasLuggage,
+                                                 @RequestParam(name = "departureDate") String date,
+                                                 @RequestParam(name = "distance") String distance,
+                                                 @RequestParam(name = "age") String age
                                                   ){
-        if(hasLuggage.equals("true") || hasLuggage.equals("false")){
-            
+        // Converting the query parameters to their real data type.
+        boolean hasLuggageFinal = hasLuggage.equals("true");
+        Instant dateFinal = ZonedDateTime.parse(date).toInstant();
+        double distanceFinal = Double.parseDouble(distance);
+        int ageFinal = Integer.parseInt(age);
+
+        boolean isValidRequest = false;
+        String errorMsg = "Invalid query parameters.";
+        double finalPrice = 0;
+
+        // Validation logic
+        if (dateFinal.isBefore(Instant.now())){
+            errorMsg = "The departure date specified is a date in the past.";
+        } else if(ageFinal <= 0) {
+            errorMsg = "The age of the passenger can't be 0 or less years old.";
+        } else if(distanceFinal <= 0) {
+            errorMsg = "The distance can't be 0 or less kilometers.";
+        } else {
+            isValidRequest = true;
+        }
+
+        //TODO: TDD - PRICE-CALCULATING ALGORITHM GOES HERE
+
+        if(!isValidRequest){
+            return new ResponseEntity<>(errorMsg, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(Double.toString(finalPrice), HttpStatus.OK);
         }
         return new ResponseEntity<>("The price for that flight would be: 84.21 €" + hasLuggage + departureDate + arrivalDate, HttpStatus.OK);
     }
--- gateway/src/main/java/com/airtrips/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/airtrips/gateway/GatewayApplication.java
@@ -2,14 +2,11 @@ package com.airtrips.gateway;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.*;
-import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerFilterFactory;
-import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerResilience4JFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
