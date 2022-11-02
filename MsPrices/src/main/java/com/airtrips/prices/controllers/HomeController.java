c29ad1b0939493376541b2e636e39d7c75de75cc
INT\Fernando.Giron <fernando.giron@solera.com>
2022-11-02 20:27:12 +0100

    Completed interface and error handling of /price endpoint logic

--- MsPrices/src/main/java/com/airtrips/prices/controllers/HomeController.java
+++ MsPrices/src/main/java/com/airtrips/prices/controllers/HomeController.java
@@ -3,10 +3,7 @@ package com.airtrips.prices.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.time.ZonedDateTime;
@@ -15,14 +12,21 @@ import java.time.ZonedDateTime;
 public class HomeController {
 
     @GetMapping("/api/v1/price")
-    public ResponseEntity<String> calculatePrice(@RequestParam(name = "hasLuggage") String hasLuggage,
-                                                 @RequestParam(name = "departureDate") String date,
-                                                 @RequestParam(name = "distance") String distance,
-                                                 @RequestParam(name = "age") String age
+    @ResponseBody
+    public ResponseEntity<String> calculatePrice(@RequestParam(required = false, value = "hasLuggage") String hasLuggage,
+                                                 @RequestParam(required = false, value = "date") String date,
+                                                 @RequestParam(required = false, value = "distance") String distance,
+                                                 @RequestParam(required = false, value = "age") String age
                                                   ){
         // Converting the query parameters to their real data type.
         boolean hasLuggageFinal = hasLuggage.equals("true");
-        Instant dateFinal = ZonedDateTime.parse(date).toInstant();
+        Instant dateFinal = null;
+        try{
+            dateFinal = Instant.parse(date);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Date supplied is in a non-accepted format. The accepted format is ISO-8601", HttpStatus.BAD_REQUEST);
+        }
+
         double distanceFinal = Double.parseDouble(distance);
         int ageFinal = Integer.parseInt(age);
 
@@ -44,10 +48,14 @@ public class HomeController {
         //TODO: TDD - PRICE-CALCULATING ALGORITHM GOES HERE
 
         if(!isValidRequest){
-            return new ResponseEntity<>(errorMsg, HttpStatus.OK);
+            return new ResponseEntity<>(errorMsg, HttpStatus.BAD_REQUEST);
         } else {
-            return new ResponseEntity<>(Double.toString(finalPrice), HttpStatus.OK);
+            //return new ResponseEntity<>(Double.toString(finalPrice), HttpStatus.OK);
+            return new ResponseEntity<>("Data received:" +
+                     "\nhasLuggageFinal: " + hasLuggageFinal
+                   + "\ndateFinal: " + dateFinal
+                   + "\ndistanceFinal: " + distanceFinal
+                   + "\nageFinal: " + ageFinal, HttpStatus.OK);
         }
-        return new ResponseEntity<>("The price for that flight would be: 84.21 €" + hasLuggage + departureDate + arrivalDate, HttpStatus.OK);
     }
 }
