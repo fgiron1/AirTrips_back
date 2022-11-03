c100cb03a6f7afec34a5c1d608e592e1908f469a
Leuri <leuri77@gmail.com>
2022-11-03 12:45:36 +0100

    CustomerSerciveTests done

--- MsFlight/src/main/java/com/airtrips/MsFlight/Repository/FlightRepository.java
+++ MsFlight/src/main/java/com/airtrips/MsFlight/Repository/FlightRepository.java
@@ -3,6 +3,7 @@ package com.airtrips.MsFlight.Repository;
 import com.airtrips.MsFlight.models.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
@@ -18,6 +19,10 @@ public interface FlightRepository extends JpaRepository<Flight, UUID> {
     @Query(value ="SELECT * FROM flights WHERE layover=:layoverNumber", nativeQuery = true)
     Stream<Flight> filterByScales(int layoverNumber);
 
-    @Query(value ="SELECT * FROM flights WHERE departureDate=:flightDepartureDate", nativeQuery = true)
-    Stream<Flight> getFligths(Instant flightDepartureDate);
+    @Query(value ="SELECT * FROM flights WHERE departureDate=:firstFlightDepartureDate", nativeQuery = true)
+    Stream<Flight> getFligths(Instant firstFlightDepartureDate);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM find_flights_by_date(:desired_date, :origin, :destination)")
+    String getFlightsByDate(@Param("desired_date") Instant desired_date,
+                            @Param("origin") UUID origin, @Param("destination") UUID destination );
 }
