52c34e46753b735269537518628b953d7e9742d1
Victor.Irimies <victoririmies99@gmail.com>
2022-11-03 09:57:52 +0100

    Airport Repository

--- MsFlight/src/main/java/com/airtrips/MsFlight/Repository/AirportInterface.java
+++ MsFlight/src/main/java/com/airtrips/MsFlight/Repository/AirportInterface.java
@@ -1,13 +1,20 @@
 package com.airtrips.MsFlight.Repository;
 
 import com.airtrips.MsFlight.models.Airport;
+import com.airtrips.MsFlight.models.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Repository
 public interface AirportInterface extends JpaRepository<Airport, UUID> {
+
+    @Query(value = "SELECT * FROM Airport WHERE origin=:origin", nativeQuery = true)
+    Stream<Airport> filterByCountry(String origin);
+
 
 }
