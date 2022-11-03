12d286be307153bb12c6d8499a872190dbc57fc5
Leuri <leuri77@gmail.com>
2022-11-03 10:29:25 +0100

    Repository

--- MsFlight/src/main/java/com/airtrips/MsFlight/Repository/FlightRepository.java
+++ MsFlight/src/main/java/com/airtrips/MsFlight/Repository/FlightRepository.java
@@ -10,16 +10,14 @@ import java.util.UUID;
 import java.util.stream.Stream;
 
 @Repository
-public interface FlightInterface extends JpaRepository<Flight, UUID> {
+public interface FlightRepository extends JpaRepository<Flight, UUID> {
 
-    @Query(value = "SELECT * FROM Flight WHERE hasLuggage=:hasLuggage", nativeQuery = true)
-    Stream<Flight> filterByLuggage(boolean hasLuggage);
-    @Query(value="SELECT * FROM Flight WHERE airline=:airline", nativeQuery = true)
+    @Query(value="SELECT * FROM flights WHERE airline=:airline", nativeQuery = true)
     Stream<Flight> filterByAirLine(String airline);
 
-    @Query(value ="SELECT * FROM Flight WHERE layover=:layover", nativeQuery = true)
+    @Query(value ="SELECT * FROM flights WHERE layover=:layover", nativeQuery = true)
     Stream<Flight> filterByScales(int layoverNumber);
 
-    @Query(value ="SELECT * FROM Flight WHERE departureDate=:departureDate", nativeQuery = true)
+    @Query(value ="SELECT * FROM flights WHERE departureDate=:departureDate", nativeQuery = true)
     Stream<Flight> getFligths(Instant firstFlightDepartureDate);
 }
