c4b07b0eb2a40fb97348c2ea5bc9698a6151067a
Leuri <leuri77@gmail.com>
2022-11-04 16:19:00 +0100

    Test for calculatePrice function done

--- MsPrices/src/test/java/com/airtrips/prices/PricesApplicationTests.java
+++ MsPrices/src/test/java/com/airtrips/prices/PricesApplicationTests.java
@@ -1,13 +1,25 @@
 package com.airtrips.prices;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.time.Instant;
 
 @SpringBootTest
 class PricesApplicationTests {
+	@Autowired
+	private PriceService service;
+	private final boolean HAS_LUGGAGE = true;
+	private final int AGE = 1;
+	private final int DISTANCE = 1000;
+	private final Instant DATE = Instant.now();
 
 	@Test
-	void contextLoads() {
+	void CalculatePrice_ReturnsNewValue_WhenUsingValidPrice() {
+		int price = service.calculatePrice(HAS_LUGGAGE, AGE, DISTANCE, DATE );
+		//
+		Assert.isTrue(price != 200, "The returned price is diferent than 200 € (base price)");
 	}
-
 }
